741603d7019f434d3ff0ea972f48e7aa7244f805
EmilianoJZX100 <5083@holbertonschool.com>
2022-11-22 19:06:58 -0300

    fix tests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -4,7 +4,8 @@ import unittest
 from models.base_model import BaseModel
 from models import storage
 import os
-
+from models.engine import file_storage
+from models.engine import db_storage
 
 class test_fileStorage(unittest.TestCase):
     """ Class to test the file storage method """
