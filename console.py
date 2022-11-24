e932954e98b18f3493730ffa7a701717e87f334a
EmilianoJZX100 <5083@holbertonschool.com>
2022-11-24 17:16:46 -0300

    def do create

--- console.py
+++ console.py
@@ -113,21 +113,37 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
-    def do_create(self, args):
+    def do_create(self, arg):
         """ Create an object of any class"""
-        words = args.split()
-        if len(words) == 0:
-            print("** class name missing **")
-            return
         try:
-            obj = HBNBCommand.classes[words[0]]()
-            obj.save()
+            if not arg:
+                raise SyntaxError()
+            my_list = line.split(" ")
+
+            kwargs = {}
+            for i in range(1, len(my_list)):
+                key, value = tuple(my_list[i].split("="))
+                if value[0] == '"':
+                    value = value.strip('"').replace("_", " ")
+                else:
+                    try:
+                        value = eval(value)
+                    except (SyntaxError, NameError):
+                        continue
+                kwargs[key] = value
+
+            if kwargs == {}:
+                obj = eval(my_list[0])()
+            else:
+                obj = eval(my_list[0])(**kwargs)
+                storage.new(obj)
             print(obj.id)
-        except Exception:
+            obj.save()
+
+        except SyntaxError:
+            print("** class name missing **")
+        except NameError:
             print("** class doesn't exist **")
-        storage.save()
-        print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
