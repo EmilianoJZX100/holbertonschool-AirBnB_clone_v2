278dad572c727adb743eb4df5a900c6d283a8692
EmilianoJZX100 <5083@holbertonschool.com>
2022-11-24 18:42:43 -0300

    console fix

--- console.py
+++ console.py
@@ -2,14 +2,16 @@
 """ Console Module """
 import cmd
 import sys
+from shlex import split
 from models.base_model import BaseModel
-from models.__init__ import storage
+from models import storage
 from models.user import User
 from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+from datetime import datetime
 
 class HBNBCommand(cmd.Cmd):
     """ Contains the functionality for the HBNB console"""
